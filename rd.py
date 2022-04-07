eba2509208cdaec03c2f6c8df92dd6e6bdd82108
MrxTools <103169446+MrxTool@users.noreply.github.com>
2022-04-07 16:44:57 +0700

    Update rd.py

--- rd.py
+++ rd.py
@@ -1,11 +1,4 @@
-print '\nWhat\'s your name ?', 
-name = raw_input('--> ')
-print '\nHow old are you, %s?' % name,
-age = int(raw_input())
-print '\nHow tall are you (in cms), %s?' % name,
-height = int(raw_input())
-print '\nHow much do you weigh (in kgs), %s?' % name,
-weight = int(raw_input())
-
-print '\nSo, %s is %d years old, %d cms tall and weighs %d kgs.\n' %(
-name, age, height, weight)
+fd=[]
+bk = raw_input("Masukkan Jumla: ")
+if(bk>len(fd)):
+print("Selamat")
