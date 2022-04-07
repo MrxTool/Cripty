6c901b1d927ae0d1e97d9c569f835faf15c9beb7
MrxTools <103169446+MrxTool@users.noreply.github.com>
2022-04-07 13:52:32 +0700

    Update rd.py

--- rd.py
+++ rd.py
@@ -1,5 +1,11 @@
-D = 
-Sf= 
-As=D*Sf
-Fds=raw_input("Kalikan: "+As)
-print(Fds)
+print '\nWhat\'s your name ?', 
+name = raw_input('--> ')
+print '\nHow old are you, %s?' % name,
+age = int(raw_input())
+print '\nHow tall are you (in cms), %s?' % name,
+height = int(raw_input())
+print '\nHow much do you weigh (in kgs), %s?' % name,
+weight = int(raw_input())
+
+print '\nSo, %s is %d years old, %d cms tall and weighs %d kgs.\n' %(
+name, age, height, weight)
